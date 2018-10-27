747d6340afecef3d53b710041d035b43aad3feac
numangilani1998 <32116269+numangilani@users.noreply.github.com>
2018-10-27 16:45:13 -0500

    java doc update

--- CritterWorld.java
+++ CritterWorld.java
@@ -4,6 +4,10 @@ import java.util.*;
 
 public class CritterWorld {
 
+	
+	/**
+	 * ArrayLists to keep track of current critter population and babies
+	 */
 	public static List<Critter> population = new ArrayList<Critter>();
 	public static List<Critter> babies = new ArrayList<Critter>();
 	
